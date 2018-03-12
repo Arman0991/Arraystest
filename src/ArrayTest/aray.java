27c6124b1d488d4dda422fce0767148935028923
Arman0991 <33642672+Arman0991@users.noreply.github.com>
2018-03-12 16:04:29 -0400

    arraytest

---
+++ src/ArrayTest/aray.java
@@ -0,0 +1,12 @@
+package ArrayTest;
+
+public class aray {
+	public static void main(String[] args) {
+
+		int[] num = { 2, 3, 4, 5 };
+
+		for (int n : num) {
+			System.out.print(n + ",");
+		}
+	}
+}
